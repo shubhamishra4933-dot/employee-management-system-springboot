96986236c6d6b131fbc17e886c04251bf7734278
shubhamishra4933-dot <shubhamishra4933@gmail.com>
2026-06-18 14:18:09 +0530

    Create User entity with JPA annotations

---
+++ src/main/java/com/ems/entity/User.java
@@ -0,0 +1,22 @@
+package com.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String role;
+}
